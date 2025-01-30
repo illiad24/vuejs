7c7ca7cb1ec1f68673c11260fc5b68b1fbc0fb89
web developer <119691181+illiad24@users.noreply.github.com>
2025-01-30 18:58:13 +0200

    Add files via upload

---
+++ task_2/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <MainLayout />
+</template>
+
+<script>
+import MainLayout from "./components/Layout";
+export default {
+  name: "App",
+  components: {
+    MainLayout,
+  },
+};
+</script>
+
+<style></style>
